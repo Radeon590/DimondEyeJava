d2edc69bd3918cacd039fb157691fdbd0d34fa64
Rodion <59920979+Radeon590@users.noreply.github.com>
2023-03-25 20:33:38 +0300

    drawer logic

--- app/src/main/java/com/example/dimondeyejava/MainActivity.java
+++ app/src/main/java/com/example/dimondeyejava/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.dimondeyejava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void switchMainLayoutDrawer(View view){
+        DrawerLayout drawerLayout = findViewById(R.id.main_layout_drawer);
+        if(!drawerLayout.isOpen()){
+            drawerLayout.open();
+        }
+        else{
+            drawerLayout.close();
+        }
     }
 }
