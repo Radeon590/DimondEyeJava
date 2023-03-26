a07da2fc578d35d5c8aad60746052234eb1ccc3c
Rodion <59920979+Radeon590@users.noreply.github.com>
2023-03-26 15:04:34 +0300

    file picker intent

--- app/src/main/java/com/example/dimondeyejava/VideoManagerActivity.java
+++ app/src/main/java/com/example/dimondeyejava/VideoManagerActivity.java
@@ -2,18 +2,36 @@ package com.example.dimondeyejava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import java.net.URI;
 
 public class VideoManagerActivity extends AppCompatActivity {
-
+    private static final int SELECT_VIDEO = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_manager);
     }
 
-    public void showGallery(View view){
+    public void showFiles(View view){
+        Intent fileSearcherIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        fileSearcherIntent.setType("video/*");
+        startActivityForResult(fileSearcherIntent, SELECT_VIDEO);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        //
+        if(requestCode == SELECT_VIDEO && resultCode == RESULT_OK){
+            Uri fileURI = imageReturnedIntent.getData();
+            Button button = findViewById(R.id.back_button);
+            button.setText(fileURI.getPath());
+        }
     }
 }
